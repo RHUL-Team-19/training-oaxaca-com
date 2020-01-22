14928644b4f0bc51879fb533b9fc1e64416456e4
izexi <43889168+izexi@users.noreply.github.com>
2020-01-22 14:10:16 +0000

    ref: replace js with ts(x) (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-import './style/style.scss';
-
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -23,6 +21,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
