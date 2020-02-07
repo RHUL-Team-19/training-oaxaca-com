7255148ac048c6b4a09469c2ff97df9ceb60b24b
MegiLB <zfac036@live.rhul.ac.uk>
2020-02-07 00:52:40 +0000

    MenuGame in progress
    added a temp image for now, and buttons need tweeking a bit.
    However, a refactor is needed in all classes so far as styling in each component needs to be moved to style.css

--- src/screens/MenuLearningGame.js
+++ src/screens/MenuLearningGame.js
@@ -2,16 +2,44 @@ import React, { Component } from "react";
 import styled, { css } from "styled-components";
 import { Link } from "react-router-dom";
 
-function MenuLearningGame(props){
-    return(
-        <Rect>
-            <Rect2>
-                <MenuLearningMessage>
-                    This is where the Menu learning game training happens! Answer as many questions correctly as you can.
-                </MenuLearningMessage>
-            </Rect2>
-        </Rect>
-    )
+function MenuLearningGame(props, image, ans1, ans2, ans3){
+  return(
+    <Rect>
+      <Rect2>
+        <MenuLearningMessage>
+          <div>
+            This is where the Menu learning game training happens! Answer as many questions correctly as you can.
+          </div>
+        </MenuLearningMessage>
+        
+        <Group>
+          <div>
+            <Image src={require("../assets/images/Menu.jpg")}></Image>
+          </div>
+        </Group>
+
+        <ButtonsRow>
+          <div>
+            <Button>
+              <ButtonOverlay>
+              {testStringVar}
+              </ButtonOverlay>
+            </Button>
+            <Button>
+              <ButtonOverlay>
+                temps
+              </ButtonOverlay>
+            </Button>
+            <Button>
+              <ButtonOverlay>
+                temps
+              </ButtonOverlay>
+            </Button>
+            </div>
+        </ButtonsRow>
+      </Rect2>
+    </Rect>
+  )
 }
 
 const Rect = styled.div`
@@ -31,7 +59,14 @@ const Rect2 = styled.div`
   margin-top: 147px;
   margin-left: 142px;
 `;
-
+const ButtonsRow = styled.div`
+  height: 38px;
+  flex-direction: row;
+  display: flex;
+  margin-top: 42px;
+  margin-left: 117px;
+  margin-right: 145px;
+`;
 const ButtonOverlay = styled.button`
  display: block;
  background: none;
@@ -40,7 +75,20 @@ const ButtonOverlay = styled.button`
  border:none
  `;
 
- const MenuLearningMessage = styled.span`
+const Button = styled.div`
+  width: 138px;
+  height: 38px;
+  padding:15px;
+  background-color: rgba(220,13,13,1);
+  border-radius: 100px;
+  shadow-opacity: 1px;
+  flex-direction: column;
+  display: flex;
+  justify-content: center;
+  border: none;
+  `;
+
+const MenuLearningMessage = styled.span`
   font-family: Roboto;
   color: #121212;
   font-size: 20px;
@@ -49,5 +97,32 @@ const ButtonOverlay = styled.button`
   margin-top: 57px;
   align-self: center;
 `;
+
+const buttonText = styled.span`
+  font-family: Roboto;
+  color: #121212;
+  font-size: 17px;
+  font-weight: 700;
+  font-style: normal;
+  align-self: center;
+`;
+
+const Group = styled.div`
+  width: 912px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 60px;
+  align-self: center;
+  display: flex;
+`;
+const Image = styled.img`
+  width: 100%;
+  height: 300px;
+  object-fit: contain;
+`;
+
+var testStringVar = "helloooo";
+
 
 export default MenuLearningGame;
