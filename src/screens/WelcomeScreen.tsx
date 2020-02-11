244766a7a5e3daf7e6a9903a64ee6b4cb79ec6cf
MegiLB <zfac036@live.rhul.ac.uk>
2020-02-11 21:22:58 +0000

    More refactoring
    Got welcome screen to render. got rid of old classes as they are no longer being used.
    added history.ts
    updated index.ts so that it works and now loads correct screen
    got rid of other classes (i.e. app, and index.js) that are not needed.

--- src/screens/WelcomeScreen.tsx
+++ src/screens/WelcomeScreen.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import { render } from 'react-dom';
-import history from '../history';
-import styled from 'styled-components';
-import { Router } from 'react-router-dom';
+//import styled from 'styled-components';
+//import { Router } from 'react-router-dom';
 //import { redirect } from '../util/Util';
 import '../style/style.css';
+import image2 from '../assets/images/Greeting.jpg';
 
 export default class WelcomeScreen extends React.Component{
 
     handleButton () {
-        /*handlemenuGameButton fuction:
-        When menuGame button is pressed then change page to the MenuLearningGame page.
-        if class name = menuGame go there etc.
-        <button onClick={handleMenuGameButton}></button>
+        /*handleButton fuction:
+        Checks to see which button is pressed to redirct to corresponding page -->
+            - If menuGame button is pressed then change page to the MenuLearningGame page.
+            onClick={() => redirect(`menu/find/${meal_id}`)}
         **/
-       history.push('/menuLearningGame')
+       alert('CLICKED');
     };
 
     render(){
@@ -24,7 +24,27 @@ export default class WelcomeScreen extends React.Component{
         return (
             <div className="welcome-screen">
                 <h2>Welcome to the training portal! Please select the game you would like to play:</h2>
-                <button className="menuGame" onClick={this.handleButton}>Menu Learning</button>
+                
+                {/* <div className="images">
+                    <figure className="image is-80x120">
+                        <img src={require("https://objects.wsantos.net/oaxaca-com/menu/images/tacos.jpg")} alt="menuGameImage"/>
+                    </figure>
+                </div> */}
+
+                <div className="buttons">
+                    {/**
+                     * 3 buttons on page for each game.
+                     * Move into seperate tables
+                     */}
+                    <button className="menuGame" onClick={this.handleButton}>
+                        <span className="icon is-small">
+                        <i className="fas fa-edit"></i>
+                        </span>
+                        <span>Menu Learning</span>
+                    </button>
+                    <button className="alergyGame" onClick={this.handleButton}>Allergy Learning</button>
+                    <button className="greetingGame" onClick={this.handleButton}>Greetings Learning</button>
+                </div>
             </div>
         );
     }
